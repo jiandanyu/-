5198387b036bbceacb2143c8c3ee7959ed48daa7
jiandanyu <74997756+jiandanyu@users.noreply.github.com>
2022-08-23 15:29:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp']), math.floor(weather['low']), math.floor(weather['high']), weather['wind']
+  return weather['weather'], math.floor(weather['temp']), math.floor(weather['low']), math.floor(weather['high']), weather['wind'], weather['province']
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -42,8 +42,8 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 
-wea, temperature, low, high, wind = get_weather()
-data = {"curdate":{"value":curdate},"weather":{"value":wea},"temperature":{"value":temperature},"low":{"value":low},"high":{"value":high},"wind":{"value":wind},"words":{"value":get_words(), "color":get_random_color()}}
+wea, temperature, low, high, wind, province = get_weather()
+data = {"curdate":{"value":curdate},"weather":{"value":wea},"temperature":{"value":temperature},"province":{"value":province},"city":{"value":city},"low":{"value":low},"low":{"value":low},"high":{"value":high},"wind":{"value":wind},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 res1 = wm.send_template(user1_id, template_id, data)
 
