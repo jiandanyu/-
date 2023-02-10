9a8969413a2216577326add393649f552ba0d536
jiandanyu <74997756+jiandanyu@users.noreply.github.com>
2023-02-10 11:59:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,14 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp']), math.floor(weather['low']), math.floor(weather['high']), weather['wind'], weather['province']
+#   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+#   res = requests.get(url).json()
+#   weather = res['data']['list'][0]
+#   return weather['weather'], math.floor(weather['temp']), math.floor(weather['low']), math.floor(weather['high']), weather['wind'], weather['province']
+    url = "https://www.yiketianqi.com/free/day?appid=36612533&appsecret=2TX26ggS&unescape=1&cityid=" + city
+    res = requests.get(url).json()
+    weather = res[0]
+    return weather['wea'], math.floor(weather['tem']), math.floor(weather['tem_day']), math.floor(weather['tem_night']), weather['win'], weather['city']
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
